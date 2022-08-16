b989c2cc5cd1c2abd84f79620bcb5741c443f7d6
84421-Marina-Brasca <81325595+84421-Marina-Brasca@users.noreply.github.com>
2022-08-15 22:56:05 -0300

    menu
    cambios en interface

--- TP1-SIM/Menu.Designer.cs
+++ TP1-SIM/Menu.Designer.cs
@@ -34,86 +34,195 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnTestiKS = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGenerarAlea
             // 
+            this.btnGenerarAlea.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnGenerarAlea.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGenerarAlea.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnGenerarAlea.Location = new System.Drawing.Point(198, 210);
-            this.btnGenerarAlea.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGenerarAlea.ForeColor = System.Drawing.Color.Transparent;
+            this.btnGenerarAlea.Location = new System.Drawing.Point(708, 390);
+            this.btnGenerarAlea.Margin = new System.Windows.Forms.Padding(4);
             this.btnGenerarAlea.Name = "btnGenerarAlea";
-            this.btnGenerarAlea.Size = new System.Drawing.Size(256, 32);
+            this.btnGenerarAlea.Size = new System.Drawing.Size(513, 69);
             this.btnGenerarAlea.TabIndex = 0;
             this.btnGenerarAlea.Text = "Generar Aleatorios";
-            this.btnGenerarAlea.UseVisualStyleBackColor = true;
+            this.btnGenerarAlea.UseVisualStyleBackColor = false;
             this.btnGenerarAlea.Click += new System.EventHandler(this.btnGenerarAlea_Click);
             // 
             // btnTestChi
             // 
+            this.btnTestChi.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnTestChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTestChi.Location = new System.Drawing.Point(198, 257);
-            this.btnTestChi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTestChi.ForeColor = System.Drawing.Color.Transparent;
+            this.btnTestChi.Location = new System.Drawing.Point(708, 486);
+            this.btnTestChi.Margin = new System.Windows.Forms.Padding(4);
             this.btnTestChi.Name = "btnTestChi";
-            this.btnTestChi.Size = new System.Drawing.Size(256, 32);
+            this.btnTestChi.Size = new System.Drawing.Size(513, 69);
             this.btnTestChi.TabIndex = 1;
             this.btnTestChi.Text = "Test de Chi Cuadrado";
-            this.btnTestChi.UseVisualStyleBackColor = true;
+            this.btnTestChi.UseVisualStyleBackColor = false;
             this.btnTestChi.Click += new System.EventHandler(this.btnTestChi_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(73, 57);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(99, 100);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(514, 48);
+            this.label1.Size = new System.Drawing.Size(429, 40);
             this.label1.TabIndex = 2;
             this.label1.Text = "TRABAJO PRÁCTICO 1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(228, 129);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(235, 282);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(199, 36);
+            this.label2.Size = new System.Drawing.Size(268, 40);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Simulación";
+            this.label2.Text = "i m u l a c i ó n";
             // 
             // btnTestiKS
             // 
+            this.btnTestiKS.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnTestiKS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTestiKS.Location = new System.Drawing.Point(198, 303);
-            this.btnTestiKS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnTestiKS.ForeColor = System.Drawing.Color.Transparent;
+            this.btnTestiKS.Location = new System.Drawing.Point(708, 585);
+            this.btnTestiKS.Margin = new System.Windows.Forms.Padding(4);
             this.btnTestiKS.Name = "btnTestiKS";
-            this.btnTestiKS.Size = new System.Drawing.Size(256, 32);
+            this.btnTestiKS.Size = new System.Drawing.Size(513, 69);
             this.btnTestiKS.TabIndex = 4;
             this.btnTestiKS.Text = "Test de KS";
-            this.btnTestiKS.UseVisualStyleBackColor = true;
+            this.btnTestiKS.UseVisualStyleBackColor = false;
             this.btnTestiKS.Click += new System.EventHandler(this.btnTestKS_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(-1, -2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 765);
+            this.panel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(131, 551);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(348, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Salas  Santiago                        83949";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(134, 488);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(356, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Brasca Marina Constanza          84421";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(134, 519);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(351, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Spaccesi Maximiliano              84095";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Mongolian Baiti", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(131, 584);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(351, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Gomez Wannaz Santiago         82594 ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Mongolian Baiti", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(133, 619);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(335, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Niveyro Lucas                       76822";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BackgroundImage = global::TP1_SIM.Properties.Resources._1553065_blue_letter_s_alphabet_letters_icon;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(119, 219);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(125, 148);
+            this.panel3.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::TP1_SIM.Properties.Resources.sistema_1200x480;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(594, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 296);
+            this.panel1.TabIndex = 5;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(685, 413);
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(1299, 759);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnTestiKS);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnTestChi);
             this.Controls.Add(this.btnGenerarAlea);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -124,5 +233,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnTestiKS;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
     }
 }
